ee998e3b87dd8069f275d114ad546d4f889f68e6
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2024-07-11 15:02:44 +0700

    All

---
+++ overloading/Program.cs
@@ -0,0 +1,5 @@
+﻿class Calculator 
+{
+	public int   
+}
+)
---
+++ polymorph_N/Program.cs
@@ -0,0 +1,14 @@
+﻿class Transportation 
+{
+	public string type;
+	public virtual void price () //polymorph
+	{
+		Console.WriteLine( "Rp.");
+	}
+}
+class Bus : Transportation 
+{
+	public override void price(){ //polymorph
+		Console.WriteLine(" h");
+	}
+}
