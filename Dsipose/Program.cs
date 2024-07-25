37542992cb9f597c1e89aa8837f6ee4b08786edb
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2024-07-25 13:10:39 +0700

    All

---
+++ Dsipose/Program.cs
@@ -0,0 +1,26 @@
+﻿class MyClass : IDisposable
+{
+	private int[] myArray;
+	private FileStream fs;
+	private OfficeStream officeStream;
+	private bool disposedValue;
+	
+	protected virtual void Dispose(bool disposing) 
+	{
+		if(!disposedValue)
+		{
+			if(disposing)
+			{
+				myArray = null;
+			}
+			fs.Dispose();
+			officeStream = null;
+			disposedValue= true;
+		}
+	}
+	~MyClass ()
+	{
+		Dispose(disposing: false);
+		GC.SuppressFinalize(this);
+	}
+}
