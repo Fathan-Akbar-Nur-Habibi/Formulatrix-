b48e06942969512a3d9bec21c9943b2f4254894d
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2024-07-26 14:01:22 +0700

    All

---
+++ Debugger/Program.cs
@@ -0,0 +1,17 @@
+﻿class Calculator { 
+		public int add(int a, int b)
+	{ 
+		 return (a+b);
+	}
+}
+
+class Program 
+{
+	static void Main ()
+	{
+		Console.WriteLine("Program Start");
+		Calculator calculator = new (); 
+		int result = calculator.add(3,5);
+		Console.WriteLine(result);
+	}
+}
