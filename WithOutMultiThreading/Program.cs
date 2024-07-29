59e13e29045bb935022ff4a40cae6a4e745ef07e
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2024-07-29 09:04:04 +0700

    All

--- WithOutMultiThreading/Program.cs
+++ WithOutMultiThreading/Program.cs
@@ -13,20 +13,20 @@ class Program {
 	}
 	static void Print()
 	{
-		Console.WriteLine("print start");
-		Thread.Sleep(10000);
+		Console.WriteLine("print start"); 
+		Thread.Sleep(10000); //10 second
 		Console.WriteLine("Print finished");
 	}
 	static void Fax()
 	{
 		Console.WriteLine("Fax Start");
-		Thread.Sleep(11000);
+		Thread.Sleep(11000); //11 second
 		Console.WriteLine("Tax Finish");
 	}
 	static void Scan ()
 	{
 		Console.WriteLine("scan start");
-		Thread.Sleep(5000);
+		Thread.Sleep(5000); //5 second
 		Console.WriteLine("scan finish");
 	}
 }
